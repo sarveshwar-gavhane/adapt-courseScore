3823502f5fcafa5fef5d6e1adbb3eb21e644137b
Sarveshwar Gavhane <sarveshwar.gavhane@exultcorp.com>
2016-11-09 16:39:29 +0530

    added _isPercentageBased

--- js/adapt-courseScore.js
+++ js/adapt-courseScore.js
@@ -7,6 +7,7 @@ define([
 
         initialize: function(options) {
             this.score = this.model._score;
+            this._currentAllQuestions=0;
             this.render();
             var courseScoreArticles = [];
 
@@ -14,15 +15,18 @@ define([
                 if (item.has('_courseScore'))
                     courseScoreArticles.push(item);
             });
+            courseScoreArticlesResults = [courseScoreArticles.length];
 
             for (var i = 0, j = courseScoreArticles.length; i < j; i++) {
-                this.setUpCourseScore(courseScoreArticles[i]);
+                this.setUpCourseScore(courseScoreArticles[i],i);
             }
         },
 
-        setUpCourseScore: function(courseScoreArticles) {
+        setUpCourseScore: function(courseScoreArticles,index) {
             this._originalChildModels = courseScoreArticles.getChildren().models;
             this._currentQuestionComponents = courseScoreArticles.findDescendants("components").where({ _isQuestionType: true });
+            courseScoreArticlesResults[index]=this._currentQuestionComponents.length;
+            this._currentAllQuestions +=this._currentQuestionComponents.length;     
             var currentQuestionsCollection = new Backbone.Collection(this._currentQuestionComponents);
             this.listenTo(currentQuestionsCollection, 'change:_isComplete', this.calculateCourseScore);
         },
@@ -32,7 +36,15 @@ define([
             if (model.get('_isCorrect') && model.get('_questionWeight')) {
                 this.score += model.get('_questionWeight');
             }
-            this.$(".courseScoreView-score").html(this.score);
+
+            var isPercentageBased=Adapt.course.get('_courseScore')._isPercentageBased;
+            if(isPercentageBased) {
+                var percentage=(this.score*100)/this._currentAllQuestions;
+                this.$(".courseScoreView-score").html(percentage.toString().split('.')[0] +" %");
+            } else {
+                this.$(".courseScoreView-score").html("total score = " +this.score);
+            }
+           
         },
 
         render: function() {
